4814d8dc1d94f36bde2a24f1243284aa87ce10a0
benR109 <braskin@eastsideprep.org>
2024-10-22 10:48:56 -0700

    final commit

--- js/truth.js
+++ js/truth.js
@@ -1,26 +1,26 @@
 let fname = "";
 
 function green() {
-    let greetParagraph = document.getElementById("green")
+    let greetParagraph = document.getElementById("green");
 
     let lname = document.getElementById("lname").value;
     let age = document.getElementById("age").value;
-    let fname = document.getElementById("fname").value;
+    fname = document.getElementById("fname").value;  // Removed 'let'
 
-    greetParagraph.innerHTML = "Greetings" + age +"year old" + fname + " " + lname;
+    greetParagraph.innerHTML = "Greetings " + age + " year old " + fname + " " + lname;
 }
 
-function trivial1() {
-    let triviaAnswer = document.getElementById(" trivial-answer");
+function trivia1() {
+    let triviaAnswer = document.getElementById("trivia1-answer");
 
-    //Get values from form
-    let chocolateSelected = document.getElementById("chocolate").Checked;
+    // Get values from form
+    let chocolateSelected = document.getElementById("chocolate").checked;
     let tunaSelected = document.getElementById("tuna").checked;
     let honeySelected = document.getElementById("honey").checked;
 
-    //Give results based off of what they selected
+    // Give results based off of what they selected
     if (chocolateSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wromg. Sweet tooth eh?";
+        triviaAnswer.innerHTML = fname + ", you are wrong. Sweet tooth eh?";
     }
     else if (tunaSelected) {
         triviaAnswer.innerHTML = fname + ", you are wrong. Rotten tuna, yuck!";
@@ -28,5 +28,23 @@ function trivial1() {
     else if (honeySelected) {
         triviaAnswer.innerHTML = fname + ", you are correct!!";
     }
+}
 
-} 
+function truth() {
+    let truthAnswer = document.getElementById("truth-answer");
+
+    let truth1Selected = document.getElementById("truth1").checked;
+    let lie1Selected = document.getElementById("lie1").checked;
+    let truth2Selected = document.getElementById("truth2").checked;
+
+   
+    if (truth1Selected) {
+        truthAnswer.innerHTML = fname + ", you are wrong. Always loved the snow and cold";
+    }
+    else if (lie1Selected) {
+        truthAnswer.innerHTML = fname + ", you are correct!!";
+    }
+    else if (truth2Selected) {
+        truthAnswer.innerHTML = fname + ", you are wrong. My family loves to travel and see new places";
+    }
+}
